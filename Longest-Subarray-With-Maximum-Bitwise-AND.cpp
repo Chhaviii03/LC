8da3447f78de1bbb8891840856440baabff3f12d
Chhavi <161047039+Chhaviii03@users.noreply.github.com>
2025-07-30 10:14:16 +0530

    [2419] [Time Beats: 53.35%] [Memory Beats: 65.24%] - LeetPush

---
+++ Longest-Subarray-With-Maximum-Bitwise-AND.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int maxVal = *max_element(nums.begin(), nums.end());
+        int longest = 0, count = 0;
+
+        for (int num : nums) {
+            if (num == maxVal) {
+                count++;
+                longest = max(longest, count);
+            } else {
+                count = 0;
+            }
+        }
+
+        return longest;
+    }
+};
